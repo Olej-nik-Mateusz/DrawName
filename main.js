9eba38503b566b2c7ea37113e70018e0ceade7f9
Mateusz Olejnik <olejuss@gmail.com>
2021-02-20 18:24:26 +0100

    add div  html/CSS, add button-draw feature in .js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const btn = document.querySelector("button")
-
-btn.addEventListener(`click`, () => {})
+const div = document.querySelector("div")
+const names = ["Annie", "Alice", "Anastazia", "Albert", "Bethany", "Tom", "Mathiew", "Max", "Billy", "Lydia", "Samuel"];
+console.log(Math.random())
+const nameGenerator = () => {
+    const index = (Math.floor(Math.random() * names.length))
+    console.log(names[index])
+    div.textContent = `A great name for Your kid is ${names[index]}`
+}
+btn.addEventListener(`click`, nameGenerator)
