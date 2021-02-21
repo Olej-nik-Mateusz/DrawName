0a0c144bceabbdb7a1157c90b3085fda4f9cb3c5
Mateusz Olejnik <olejuss@gmail.com>
2021-02-21 13:18:18 +0100

    new feature - addName, move style into style.css

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DrawName
-Click button --> Draw Name
+1) Write a name
+2) Click "Add Name" button to addit to the pot
+3) Before You click button "Draw Name", add all names You have in mind
+4) App will pop a name from pot You've created. 
--- main.js
+++ main.js
@@ -1,11 +1,30 @@
-const btn = document.querySelector("button")
+const btnForm = document.querySelector(".form")
+const btnDraw = document.querySelector(".draw")
 const div = document.querySelector("div")
-const names = ["Annie", "Alice", "Anastazia", "Albert", "Bethany", "Tom", "Mathiew", "Max", "Billy", "Lydia", "Samuel"];
+const newName = document.querySelector(".addName")
+const names = [];
 const firstLine = ["A great name for Your kid is", "You should choose", "The best choice seems to be", "There will be lot of fun with", "The greatest - "]
-console.log(Math.random())
+
+
+
+const addName = (e) => {
+    e.preventDefault()
+    const input = document.querySelector(`input`)
+    const name = input.value
+    console.log(input.value)
+    if (input.value) {
+        names.push(name)
+        console.log(names)
+        newName.textContent += name + ","
+        input.value = ""
+    }
+}
+
+
 const nameGenerator = () => {
     const index = (Math.floor(Math.random() * names.length))
     // console.log(names[index])
     div.textContent = `${firstLine[(Math.floor(Math.random()*firstLine.length))]} ${names[index]}`
 }
-btn.addEventListener(`click`, nameGenerator)
+btnForm.addEventListener(`click`, addName)
+btnDraw.addEventListener(`click`, nameGenerator)
